d574dc14085ea6349f113978f74de62061391914
John Musca <warlockjm@gmail.com>
2015-11-21 18:07:10 +1100

    sanity check, comments

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,20 @@
+/**
+*  Main controller. Ensures form is valid then redirects to next page.
+*
+*  @param $location    The location provider.
+*  @param loginService The login data service, used to persist data across controllers.
+*/
 demoApp.controller('mainController', function($scope, $location, loginService) 
 {
+  
+  /**
+  *  Handles submit.
+  * 
+  *  @param path       The path to redirect to if sucessful.
+  *  @param form_valid The validity of the form, passed from the form.
+  *
+  *  @return boolean     Returns true or false if form is valid or not. 
+  */
   $scope.submitFunction = function(path, form_valid) {
 
     if(form_valid) {
@@ -13,6 +28,11 @@ demoApp.controller('mainController', function($scope, $location, loginService)
   };
 });
 
+/**
+*  Thanks controller. Displays a thank you message
+*
+*  @param loginService The login data service, used to persist data across controllers.
+*/
 demoApp.controller('thanksController', function($scope, loginService) 
 {
   $scope.message = 'Well hello there ' + loginService.getLoginResponse().login_name + '.';
