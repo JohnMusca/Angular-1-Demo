f17a6dfbd9bd5367811e2760703229f6eb609dad
John Musca <warlockjm@gmail.com>
2015-11-21 14:12:30 +1100

    fixed up tests for directives

--- js/directives.js
+++ js/directives.js
@@ -5,16 +5,27 @@ demoApp.directive('validateName', function() {
     link: function(scope, elm, attrs, ctrl) {
       ctrl.$parsers.unshift(function(viewValue) {
         
-        if (viewValue == 'John') {
-          // it is valid
-          ctrl.$setValidity('integer', true);
-          return viewValue;
-        }
+        /*
+        var allowed_names = ['John', 'Peter'];
         
-        if (viewValue == 'Peter') {
+        var name_passed = allowed_name.filter(function(allowed_names) {
+                            return allowed_names === allowed_names;
+                          });
+                          
+        console.log(name_passed);
+        */
+        
+        if (viewValue === 'John') {
           // it is valid
-          ctrl.$setValidity('integer', true);
+          ctrl.$setValidity('validName', true);
           return viewValue;
+        } else if (viewValue === 'Peter') {
+          // it is valid
+          ctrl.$setValidity('validName', true);
+          return viewValue;
+        } else {
+          ctrl.$setValidity('validName', false);
+          return undefined;
         }
          
       });
--- js/tests/spec/DirectiveSpec.js
+++ js/tests/spec/DirectiveSpec.js
@@ -6,15 +6,17 @@ describe('directives', function() {
     $scope = $rootScope;
     var element = angular.element(
       '<form name="form">' +
-      '<input ng-model="model.login_name" name="login_name" validateName />' +
+      '<input ng-model="model.login_name" name="login_name" validate-name />' +
       '</form>'
     );
+    
     $scope.model = { login_name: null }
     $compile(element)($scope);
     form = $scope.form;
   }));
 
   describe('validateName', function() {
+    
     //test for John
     it('should pass with John', function() {
       form.login_name.$setViewValue('John');
@@ -35,7 +37,7 @@ describe('directives', function() {
     it('should not pass with Paul', function() {
       form.login_name.$setViewValue('Paul');
       $scope.$digest();
-      expect($scope.model.login_name).toEqual('Paul');
+      expect($scope.model.login_name).toBeUndefined();
       expect(form.login_name.$valid).toBe(false);
     });
     
