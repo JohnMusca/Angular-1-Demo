801dfe23be0c0949b5d1b7e93b276f3c39bfcdc5
John Musca <warlockjm@gmail.com>
2015-11-21 14:32:47 +1100

    directive needs some work, to come back to

--- js/directives.js
+++ js/directives.js
@@ -1,33 +1,31 @@
-
+/**
+*  Directive to validate name.
+*
+*  @param string login_name The login name.
+*
+*  @return Mixed sets return to undefined or the name entered if valid.
+*/
 demoApp.directive('validateName', function() {
   return {
     require: 'ngModel',
     link: function(scope, elm, attrs, ctrl) {
       ctrl.$parsers.unshift(function(viewValue) {
-        
-        /*
+
         var allowed_names = ['John', 'Peter'];
         
-        var name_passed = allowed_name.filter(function(allowed_names) {
-                            return allowed_names === allowed_names;
+        //high order function to determine if name is allowed
+        var name_passed = allowed_names.filter(function(allowed_names) {
+                            return allowed_names === viewValue;
                           });
-                          
-        console.log(name_passed);
-        */
-        
-        if (viewValue === 'John') {
-          // it is valid
+
+        if(name_passed.length !== 0) {
           ctrl.$setValidity('validName', true);
           return viewValue;
-        } else if (viewValue === 'Peter') {
-          // it is valid
-          ctrl.$setValidity('validName', true);
-          return viewValue;
-        } else {
-          ctrl.$setValidity('validName', false);
-          return undefined;
         }
-         
+        
+        ctrl.$setValidity('validName', false);
+        return undefined;
+
       });
     }
   };
